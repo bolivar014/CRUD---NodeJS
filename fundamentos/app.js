53d352dc57feba20515167d3b66c7d6df029619b
Camilo Valdes B <bolivar014@gmail.com>
2022-05-06 12:36:38 -0500

    fundamentos de NODEJS

---
+++ fundamentos/app.js
@@ -0,0 +1,18 @@
+// Inicializamos variables
+const frutas = ['platano', 'manzana', 'pera', 'melocoton', 'platano'];
+let cont = 0;
+
+// iteramos registros dentro del array frutas
+frutas.forEach(item => {
+    // Contador de iteracciones en el ciclo
+    cont += 1;
+
+    // Imprimimos en pantalla
+    console.log('el valor en posición : ' + cont + ', es: ' + item);
+
+    // Imprimimos
+    console.log('cantidad de registros repetidos...');
+    
+    // Imprimimos valores repetidos
+    console.count(item);
+})
